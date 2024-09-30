60996c4fc8b85d500caa13907bc3d66ce600dfa8
Arnaud Kandala <arnaudkandala@gmail.com>
2024-09-30 16:34:13 -0400

    final

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,12 @@
 export default function Index() {
   return (
     <p id="index-page">
+      <h1>Micro Frontend federator</h1>
       This is a demo for Remix.
       <br />
       Check out <a href="https://remix.run">the docs at remix.run</a>.
       <br />
-      <h1>Please check the CSP-NONCE to see if it worked !!!</h1>
+      <h3>Please check the CSP-NONCE to see if it worked !!!</h3>
     </p>
   );
 }
